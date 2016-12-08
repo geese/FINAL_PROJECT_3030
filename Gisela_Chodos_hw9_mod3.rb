3d1b81efb0b7c81540f804d0a4d7f1ee96a22188
Gisela Chodos <gisela.chodos@gmail.com>
2016-12-08 14:07:22 -0700

    mod 3 done

--- Gisela_Chodos_hw9_mod3.rb
+++ Gisela_Chodos_hw9_mod3.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 
 
-barcode_table = {1=>'00011', 2=>'00101',3=>'00110', 4=>'01001', 5=>'01010', 6=>'01100', 7=>'10001', 8=>'10010', 9=>'10100', 0=>'11000'}
 
-
+# prompt the user to enter a zip code.
+#
+# returns:   an array of six integers. The first five digits are the zip code,
+# 			 the last digit is the checksum.
+#
 def printDigit
 	puts "Please enter a zip code."
 	zip = gets
@@ -32,4 +35,34 @@ def printDigit
 	return zip_array
 end
 
-printDigit
+
+# Prints decimal zipcode and check value as a barcode
+# using pseudo-binary.
+#
+# Output Format:
+# 	| xxxxx xxxxx xxxxx xxxxx xxxxx xxxxx |
+# 	where x = either "|" = 1 or ":" = 0
+# 
+# zip_array - Decimal zipcode with check number
+#
+# Prints:  6 decimal digits expressed as pseudo-binary barcode
+#
+
+def printBarCode(zip_array)
+	barcode_table = {1=>'00011', 2=>'00101',3=>'00110', 4=>'01001', 5=>'01010', 6=>'01100', 7=>'10001', 8=>'10010', 9=>'10100', 0=>'11000'}
+	barcode = "|"
+	zip_array.each{
+		|digit|  # each digit is a string
+		code = barcode_table[digit.to_i].scan /\d/
+		code.each{
+			|bit|
+			bit == "0" ? barcode += ":" : barcode += "|"
+		}
+	}
+	barcode += "|"
+	puts barcode
+end
+
+
+theZip = printDigit
+printBarCode(theZip)
